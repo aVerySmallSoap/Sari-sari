bd7d017b48edc228c8c851d60dac5fe020e756da
Lirys <iamyourmarkieboo@gmail.com>
2023-04-21 05:48:27 +0800

    Change button icons to 48x48

--- src/UI/Custom/Components/iconButton.java
+++ src/UI/Custom/Components/iconButton.java
@@ -6,7 +6,7 @@ import java.awt.*;
 public class iconButton extends JButton {
 
     public iconButton(String path){
-        Image icon = new ImageIcon(getClass().getResource(path)).getImage().getScaledInstance(32,32, Image.SCALE_SMOOTH);
+        Image icon = new ImageIcon(getClass().getResource(path)).getImage().getScaledInstance(48,48, Image.SCALE_SMOOTH);
         this.setIcon(new ImageIcon(icon));
         this.setFocusPainted(false);
     }
